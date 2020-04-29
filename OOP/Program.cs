70df1e15b5899a38678c1750b213d6c012b8405b
Mei <fongmeiw@gmail.com>
2020-04-29 06:01:14 -0500

    Inheritance

---
+++ OOP/Program.cs
@@ -0,0 +1,13 @@
+﻿,using System;
+wertyu8o]= bml'qsdfhjl']23467804`  
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
